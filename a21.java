c3da3b747ecc89dedabc31ffcd1401d3d6427564
小人物 <51994274+dingqiwen@users.noreply.github.com>
2019-11-25 10:44:26 +0800

    Java文件
    练手

---
+++ a21.java
@@ -0,0 +1,14 @@
+class a21 
+{
+	public static void main(String[] args) 
+	{
+		int b=getb(3,5);
+
+		System.out.println(b);
+	}
+	public static int getb(int a,int b){
+		int sum=a*b;
+		return sum;
+	
+	}
+}
